2e4997ba5bc0883c261912486860191b90cd86ba
Evan Vandenberg <vandem02@uwgb.edu>
2023-06-27 21:34:14 -0500

    Added timer

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,19 @@
-var homeButtonEl = $("#home");
+var time = 80
+var interval
+var timeout
 
-function func1() {
-    $("#home").hide();
-    $("#q1").show;
+function start() {
+    interval = setInterval(timer, 1000)
+    timeout = setTimeout(myFunction, 80000)
+    $("#home").hide()
+    $("#q1").show()
+}
+
+function timer() {
+    time--
+    $("#time").text(time)
+}
+
+function endQuiz() {
+    
 }
