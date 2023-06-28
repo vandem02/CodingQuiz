6d086ef0e0715806c0cc42971737ceab73b16520
Evan Vandenberg <vandem02@uwgb.edu>
2023-06-28 01:16:57 -0500

    Added more questions and some CSS

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-var time = 5;
+var time = 80;
 var interval;
 
 function start() {
@@ -13,12 +13,12 @@ function timer() {
   if (time < 0) {
     time++;
     $(".time").text(time);
-    clearInterval(interval);
     endQuiz();
   }
 }
 
 function endQuiz() {
+  clearInterval(interval);
   $("#quiz").hide();
   $("#scoreboard-add").show();
 }
@@ -29,11 +29,92 @@ $("#add").on("click", function () {
     $("#scoreboard-add").hide();
     $("#scoreboard").show();
     for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        const value = localStorage.getItem(key);
-        $("table").append(
-          `<tr><td>${key}</td><td>${value}</td></tr>`
-        );
-      }
+      const key = localStorage.key(i);
+      const value = localStorage.getItem(key);
+      $("table").append(`<tr><td>${key}</td><td>${value}</td></tr>`);
+    }
   }
-})
+});
+
+function q1wrong() {
+    time -= 10
+    if (time < 0) time = 0
+    $(".time").text(time);
+  $("#feedback").text("Incorrect!");
+  $("#feedback").css("color", "red");
+  $("#q1").hide();
+  $("#q2").show();
+}
+
+function q1right() {
+  $("#feedback").text("Correct!");
+  $("#feedback").css("color", "green");
+  $("#q1").hide();
+  $("#q2").show();
+}
+
+function q2wrong() {
+    time -= 10
+    if (time < 0) time = 0
+    $(".time").text(time);
+  $("#feedback").text("Incorrect!");
+  $("#feedback").css("color", "red");
+  $("#q2").hide();
+  $("#q3").show();
+}
+
+function q2right() {
+  $("#feedback").text("Correct!");
+  $("#feedback").css("color", "green");
+  $("#q2").hide();
+  $("#q3").show();
+}
+
+function q3wrong() {
+    time -= 10
+    if (time < 0) time = 0
+    $(".time").text(time);
+  $("#feedback").text("Incorrect!");
+  $("#feedback").css("color", "red");
+  $("#q3").hide();
+  $("#q4").show();
+}
+
+function q3right() {
+  $("#feedback").text("Correct!");
+  $("#feedback").css("color", "green");
+  $("#q3").hide();
+  $("#q4").show();
+}
+
+function q4wrong() {
+    time -= 10
+    if (time < 0) time = 0
+    $(".time").text(time);
+  $("#feedback").text("Incorrect!");
+  $("#feedback").css("color", "red");
+  $("#q4").hide();
+  $("#q5").show();
+}
+
+function q4right() {
+  $("#feedback").text("Correct!");
+  $("#feedback").css("color", "green");
+  $("#q4").hide();
+  $("#q5").show();
+}
+
+function q5wrong() {
+    time -= 10
+    if (time < 0) time = 0
+    $(".time").text(time);
+  $("#feedback").text("Incorrect!");
+  $("#feedback").css("color", "red");
+  endQuiz()
+}
+
+function q5right() {
+  $("#feedback").text("Correct!");
+  $("#feedback").css("color", "green");
+  endQuiz()
+}
